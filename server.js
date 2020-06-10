065cc456b31aa64cd8ecde4cd3af2c1d257de679
Mario Bermudez Jr <mario.bermudez.jr@outlook.com>
2020-06-09 22:29:24 -0700

    Add singinAuthentication function and use existing signin function as helper function

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(morgan('combined'))
 app.get('/', (req, res) => {
   res.send("It's working!")
 })
-app.post('/signin', signin.handleSignin(db, bcrypt))
+app.post('/signin', signin.signinAuthentication(db, bcrypt))
 app.post('/register', (req, res) => {
   register.handleRegister(req, res, db, bcrypt)
 })
