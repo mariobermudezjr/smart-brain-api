5d8cf5d4abb34109693867060bcdb196549e4bb0
Mario Bermudez Jr <mario.bermudez.jr@outlook.com>
2020-06-12 15:45:54 -0700

    Add age and pet fields to database and seed data

--- controllers/profile.js
+++ controllers/profile.js
@@ -19,7 +19,7 @@ const handleProfileUpdate = (req, res, db) => {
 
   db('users')
     .where({ id })
-    .update({ name })
+    .update({ name, age, pet })
     .then((resp) => {
       if (resp) {
         res.json('success')
--- postgres/seed/seed.sql
+++ postgres/seed/seed.sql
@@ -1,6 +1,6 @@
 BEGIN TRANSACTION;
 
-INSERT into users(name, email, entries, joined) values ('Jessie', 'jessie@gmail.com', 5, '2018-01-01');
+INSERT into users(name, email, entries, joined, age, pet) values ('Jessie', 'jessie@gmail.com', 5, '2018-01-01', 25, 'dragon');
 INSERT into login(hash, email) values ('$2a$04$8y3VZnwGhWrEkja4WB0.5uQRhYf.vh8qoRm3VEOCdZMkEWEsAYwxW', 'jessie@gmail.com');
 
 COMMIT;
